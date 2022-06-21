db19e7de31aa1218aefeb47fb8aed677b3930af4
Job Siemerink <107898501+n3sty@users.noreply.github.com>
2022-06-21 12:05:59 +0200

    Uploaded progress
    Everything that I've done so far is now uploaded to GitHub

---
+++ TopDownRPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform lookat;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // This is to check if we're on the bounds of the X-axis
+        float deltaX = lookat.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookat.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        // This is to check if we're on the bounds of the X-axis
+        float deltaY = lookat.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookat.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
---
+++ TopDownRPG/Assets/Scripts/Chest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectible
+{
+    public Sprite emptyChest;
+    public int cashAmount = 5;
+
+    protected override void OnCollect()
+    {
+        if (!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChest;
+        }
+
+        Debug.Log("Grant " + cashAmount + " moneys!");
+    }
+}
